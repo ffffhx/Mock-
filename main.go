0f628f3eaa68799d7b16626859ee80d90fbbf159
Syfyivan <2578055724@qq.com>
2024-11-11 10:34:35 +0800

    test

--- main.go
+++ main.go
@@ -115,7 +115,7 @@ func main() {
 	//writerKafka(ctx)
 
 	go func() {
-		http.HandleFunc("/api/v1/messages", handleMessages)
+		http.HandleFunc("messages", handleMessages)
 		log.Println("Starting server at port 8080")
 		if err := http.ListenAndServe(":8080", nil); err != nil {
 			log.Fatal(err)
